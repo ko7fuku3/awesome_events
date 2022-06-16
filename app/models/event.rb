79ac26437aeabee90fe1999ba2a34d7d7a1176f8
k-fukushima <iikotoarusa.21th@gmail.com>
2022-06-16 21:00:58 +0000

    イベント登録機能 イベント登録機能とエラー文言表示処理の実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,12 +4,12 @@ class Event < ApplicationRecord
   validates :content, length: { maximum: 2000 }, presence: true
   validates :start_at, presence: true
   validates :end_at, presence: true
-  validates :start_at_should_be_before_end_at
+  validate :start_at_should_be_before_end_at
 
   private
 
     def start_at_should_be_before_end_at
-      return unnless start_at && end_at
+      return unless start_at && end_at
 
       if start_at >= end_at
         errors.add(:start_at, "は終了時間よりも前に設定してください")
--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module AwesomeEvents
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
   end
 end
